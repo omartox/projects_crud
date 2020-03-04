df11d0e510819321efc0ce06e60e01468fe35386
Omar Toxqui Tolama <omartox@gmail.com>
2020-03-04 00:17:42 -0600

    Revert "Finalizacion del proyecto CRUD en ejemplo alumnos"
    This reverts commit 736ab3c4e81a176479c1e3d95c184ed950951533.

--- CRUD_JAVA/src/Clases/AlumnosDB.java
+++ CRUD_JAVA/src/Clases/AlumnosDB.java
@@ -7,15 +7,12 @@ package Clases;
 
 import Utils.conexionMYSQL;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 /**
- * @Archivo AlumnosDB.java
- * @author Omar Toxqui Tolama
- * @Descripcion Clase donde se tienen las instrucciones
- * para el CRUD de datos.
+ *
+ * @author Programer
  */
 public class AlumnosDB {
     
@@ -33,109 +30,11 @@ public class AlumnosDB {
             //Creamos la sentencia
             sentencia = cn.createStatement();
             
-            datos = sentencia.executeQuery("SELECT * FROM alumnos");
+            datos = sentencia.executeQuery("SELECT * FROM alumno");
         }catch(Exception e){
             System.out.println(e);
         }
         return datos;
     }
     
-    
-    
-    
-    //funcion para insertar en la tabla
-    public boolean insertar(String Nombre, String Apellidos, int Edad, String Telefono, String Carrera){
-        boolean respuesta= false;
-        
-        Connection cn = null;
-        
-        cn = conexionMYSQL.getConexion();
-        
-        PreparedStatement preSentencia;
-        
-        try{
-            //Preparamos la sentencia para la inserción
-            preSentencia = cn.prepareStatement("insert into alumnos (nombre,apellidos,edad,telefono,carrera) values (?,?,?,?,?)");
-            
-            preSentencia.setString(1, Nombre);
-            preSentencia.setString(2, Apellidos);
-            preSentencia.setInt(3, Edad);
-            preSentencia.setString(4, Telefono);
-            preSentencia.setString(5, Carrera);
-            
-            int res = preSentencia.executeUpdate();
-            
-            if(res==1){
-                respuesta = true;
-            }else{
-                respuesta = false;
-            }
-        }catch(Exception e){
-            System.out.println(e);
-        }
-        return respuesta;
-    }
-    
-    
-    public boolean Actualizar(int Matricula, String Nombre, String Ape, int Edad, String Tel, String Carrera){
-        
-        boolean respuesta = false;
-        
-        Connection cn = null;
-        
-        cn = conexionMYSQL.getConexion();
-        PreparedStatement preSentencia;
-        // statement = sentencias resultset = datos y PeraredStatement = perar la sentencia
-            
-        try{
-            
-            preSentencia = cn.prepareStatement("UPDATE alumnos SET nombre = ?, apellidos = ?, edad = ?, telefono = ?, carrera = ? WHERE matricula = ?");
-            
-            preSentencia.setString(1, Nombre);
-            preSentencia.setString(2, Ape);
-            preSentencia.setInt(3, Edad);
-            preSentencia.setString(4, Tel);
-            preSentencia.setString(5, Carrera);
-            preSentencia.setInt(6, Matricula);
-            
-            int res = preSentencia.executeUpdate();
-                        
-            if(res==1){
-                respuesta = true;
-            }else{
-                respuesta = false;
-            }
-            
-            
-        }catch( Exception  e){
-            System.out.println(e);
-        }
-        return respuesta;
-    }
-    
-    public boolean Eliminar(int matricula){
-        boolean respuesta = false;
-        
-        
-        Connection cn = null;
-        
-        cn = conexionMYSQL.getConexion();
-        
-        PreparedStatement preSentencia;
-        
-        try{
-            preSentencia = cn.prepareStatement("DELETE FROM alumnos WHERE matricula=?");
-            preSentencia.setInt(1, matricula);
-            int res = preSentencia.executeUpdate();
-            
-            if(res== 1){
-                respuesta = true;
-            }else{
-                respuesta = false;
-            }
-        }catch(Exception e){
-            System.out.println(e);
-        }
-        return respuesta;
-    }
 }
--- CRUD_JAVA/src/Utils/conexionMYSQL.java
+++ CRUD_JAVA/src/Utils/conexionMYSQL.java
@@ -9,9 +9,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 /**
- * 
- * @author Omar Toxqui Tolama
- * 
+ *
+ * @author Programer
  */
 public class conexionMYSQL {
 
@@ -19,7 +18,7 @@ public class conexionMYSQL {
      * @param args the command line arguments
      */
  
-    //Conexion a la base de datos MYSQL
+    
     public static Connection getConexion(){
        Connection conexion = null;
         try{
